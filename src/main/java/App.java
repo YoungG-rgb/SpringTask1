c778a0981930a30264863e5933c2c4be509c4821
Zhalaldin <>
2021-08-18 04:12:58 +0600

    new

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean);
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2);
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3);
+
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4);
+    }
+}
